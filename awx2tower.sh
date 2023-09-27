4a9b72aed5bd81bccb18eb791e4c753fccaab42b
Paweł Koperwas <68131448+pkoperwas@users.noreply.github.com>
2023-09-28 00:48:02 +0200

    Update awx2tower.sh

--- awx2tower.sh
+++ awx2tower.sh
@@ -1,8 +1,8 @@
-find /var/lib/rancher/k3s/agent/containerd/ -name "favicon.ico" -exec sh -c 'rm "{}" && wget -q -O "{}" "https://github.com/pkoperwas/awx-installator/raw/main/img/favicon.ico"' \;
-find /var/lib/rancher/k3s/agent/containerd/ -name "logo-header.svg" -exec sh -c 'rm "{}" && wget -q -O "{}" "https://github.com/pkoperwas/awx-installator/raw/main/img/logo-header.svg"' \;
-find /var/lib/rancher/k3s/agent/containerd/ -name "logo-login.svg" -exec sh -c 'rm "{}" && wget -q -O "{}" "https://github.com/pkoperwas/awx-installator/raw/main/img/logo-login.svg"' \;
-find /var/lib/rancher/k3s/agent/containerd/ -name "pfbg_2000.jpg" -exec sh -c 'rm "{}" && wget -q -O "{}" "https://github.com/pkoperwas/awx-installator/raw/main/img/pfbg_2000.jpg"' \;
-find /var/lib/rancher/k3s/agent/containerd/ -name "pfbg_576.jpg" -exec sh -c 'rm "{}" && wget -q -O "{}" "https://github.com/pkoperwas/awx-installator/raw/main/img/pfbg_576.jpg"' \;
-find /var/lib/rancher/k3s/agent/containerd/ -name "pfbg_576@2x.jpg" -exec sh -c 'rm "{}" && wget -q -O "{}" "https://github.com/pkoperwas/awx-installator/raw/main/img/pfbg_576@2x.jpg"' \;
-find /var/lib/rancher/k3s/agent/containerd/ -name "pfbg_768.jpg" -exec sh -c 'rm "{}" && wget -q -O "{}" "https://github.com/pkoperwas/awx-installator/raw/main/img/pfbg_768.jpg"' \;
-find /var/lib/rancher/k3s/agent/containerd/ -name "pfbg_768@2x.jpg" -exec sh -c 'rm "{}" && wget -q -O "{}" "https://github.com/pkoperwas/awx-installator/raw/main/img/pfbg_768@2x.jpg"' \;
+find /var/lib/rancher/k3s/agent/containerd/ -name "favicon.ico" -exec cp -v img/favicon.ico {} \;
+find /var/lib/rancher/k3s/agent/containerd/ -name "logo-header.svg" -exec cp -v img/logo-header.svg {} \;
+find /var/lib/rancher/k3s/agent/containerd/ -name "logo-login.svg" -exec cp -v img/logo-login.svg {} \;
+find /var/lib/rancher/k3s/agent/containerd/ -name "pfbg_2000.jpg" -exec cp -v img/pfbg_2000.jpg {} \;
+find /var/lib/rancher/k3s/agent/containerd/ -name "pfbg_576.jpg" -exec cp -v img/pfbg_576.jpg {} \;
+find /var/lib/rancher/k3s/agent/containerd/ -name "pfbg_576@2x.jpg" -exec cp -v img/pfbg_576@2x.jpg {} \;
+find /var/lib/rancher/k3s/agent/containerd/ -name "pfbg_768.jpg" -exec cp -v img/pfbg_768.jpg {} \;
+find /var/lib/rancher/k3s/agent/containerd/ -name "pfbg_768@2x.jpg" -exec cp -v img/pfbg_768@2x.jpg {} \;
